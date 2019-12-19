fe8191df29b6b5606c1148135be073a5804e973f
Mahesh <maheshgadekar1994@gmail.com>
2019-12-19 12:02:20 +0530

    Second commit- minor changes

---
+++ src/cmdLineArgu.java
@@ -0,0 +1,14 @@
+
+public class cmdLineArgu {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=0; i<args.length;i++)
+			System.out.println("Arguments : "+args[i]);
+		
+		System.out.println("After run : "+args.length);
+
+	}
+
+}
